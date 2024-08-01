1a2f80f37e979a6983bf23162beee434aec13dfe
Christopher Magnuson <cmagnuson0489@gmail.com>
2024-08-01 12:59:52 -0700

    Changes to portfolio site

---
+++ pages/blog/id.js
@@ -0,0 +1,36 @@
+import { getAllPostIds, getPostData } from '../../lib/posts';
+import Link from 'next/link';
+import styles from '../../styles/Blog.module.css';
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export default function Post({ postData }) {
+  return (
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <h1>{postData.title}</h1>
+        <nav>
+          <Link href="/blog">Back to Blog</Link>
+        </nav>
+      </header>
+      <article className={styles.articleContent}>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </div>
+  );
+}
