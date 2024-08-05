a3934de2ef4c38d793992e58a4706817a23b540d
Christopher Magnuson <cmagnuson0489@gmail.com>
2024-08-04 20:30:11 -0700

    Submit

--- pages/api/apps.js
+++ pages/api/apps.js
@@ -1,4 +1,4 @@
-import '../styles/globals.css';
+import '../../styles/globals.css';
 
 function portfolioApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/api/index.js
+++ pages/api/index.js
@@ -1,6 +1,5 @@
 import Link from 'next/link';
 import styles from '../styles/Home.module.css';
-
 export default function Home() {
   return (
     <div className={styles.container}>
